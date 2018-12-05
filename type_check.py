970444557ffff2b83a060431b671530ae995c39a
brasspots <44511851+brasspots@users.noreply.github.com>
2018-12-05 19:11:12 +0000

    Added check for float, octal and binary

--- type_check.py
+++ type_check.py
@@ -18,7 +18,7 @@ VERSION = "1.0"
 
 # check int
 def check_int(value):
-    """asserts a given value is a valid integer
+    """ asserts a given value is a valid integer
     takes:
         STR value - the value to check
     gives:
@@ -26,15 +26,49 @@ def check_int(value):
     return all(character.isdigit() for character in value)
 
 
-# check hex
-def check_hex(value):
-    """asserts a given value is valid hex
+# check float
+def check_float(value):
+    """ asserts a given value is a valid float
     takes:
         STR value - the value to check
     gives:
-        BOOL - true is the value is valid hex, otherwise false"""
-    return all(character.upper() in ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F") for character in value)
+        BOOL - true is the value is a valid float, otherwise false
+    note: this is a strong check so integers will be rejected"""
+    return all(character.isdigit() or character == "." for character in value) and "." in value
+
+
+# check hex
+def check_hex(value):
+    """ asserts a given value is valid hexadecimal
+    takes:
+        STR value - the value to check
+    gives:
+        BOOL - true is the value is valid hexadecimal, otherwise false"""
+    return all(character.upper() in tuple(chr(dec) for dec in range(48, 58)) + tuple(chr(dec) for dec in range(65, 71)) for character in value)
+
+
+# check oct
+def check_oct(value):
+    """ asserts a given value is valid octal
+    takes:
+        STR value - the value to check
+    gives:
+        BOOL - true is the value is valid octal, otherwise false"""
+    return all(character in (chr(dec) for dec in range(48, 56)) for character in value)
+
+
+# check bin
+def check_bin(value):
+    """ asserts a given value is valid binary
+    takes:
+        STR value - the value to check
+    gives:
+        BOOL - true is the value is valid binary, otherwise false"""
+    return all(character in ("0", "1") for character in value)
 
 # initialise type dictionary
 type_dict = {"<INT>": check_int,
-             "<HEX>": check_hex}
+             "<FLOAT>": check_float,
+             "<HEX>": check_hex,
+             "<OCT>": check_oct,
+             "<BIN>": check_bin}
