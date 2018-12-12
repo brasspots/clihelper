c327429b175b37158aa004820733b9618cd936e9
brasspots <44511851+brasspots@users.noreply.github.com>
2018-12-12 17:32:15 +0000

    Added assertions for parameter information in constructor

--- clihelper.py
+++ clihelper.py
@@ -24,7 +24,7 @@ VERSION = "2.0"
 # parameter class
 class Parameter:
     # parameter constructor
-    def __init__(self, flag_alias, description, value, value_type):
+    def __init__(self, flag_alias, long_name, description, value, value_type):
         """initialises a parameter
         takes:
             STR flag_alias - the short name for the parameter
@@ -36,6 +36,7 @@ class Parameter:
 
         # copy arguments into local namespace
         self.flag = flag_alias
+        self.long_name = long_name
         self.description = description
         self.value = value
         self.value_type = value_type
@@ -47,11 +48,23 @@ class Parameter:
             BOOL strict - whether to throw an error if self.type_value is not in cores/checker.py's type dictionary
         gives:
             BOOL - True if the type matches, otherwise False"""
-        # return boolean from checker
-        return checker.type_dict.get(self.value_type, (lambda x: not strict))(self.value)
+
+        # check if value_type is iterable
+        if self.value_type[0] == "*":
+            # check all values against checker function
+            return type(self.value) == list and all(checker.type_dict.get(self.value_type, (lambda x: not strict))(value) for value in self.value)
+
+        # otherwise run the checker function once
+        else:
+            # return boolean from checker
+            return checker.type_dict.get(self.value_type, (lambda x: not strict))(self.value)
+
 
 # interface class
 class Interface:
+    # initialise control characters
+    control_characters = (" ", "|", "{", "}", "[", "]")
+
     # interface constructor
     def __init__(self, script_name, short_description, long_description, pattern_tree, parameter_information):
         """initialises interface
@@ -60,10 +73,10 @@ class Interface:
             STR short_description - a brief description of the program to be shown at the top of the help message
             STR long_description - a more in-depth description to be shown at the bottom of the help message
             DICT pattern_tree - a dictionary of patterns to use keyed using sub commands
-            ITER parameter_information - list of information about parameters in the format [flag, long name, description, default_value]
+            ITER parameter_information - list of information about parameters in the format [flag, long name, description, default_value, value_type]
         gives:
             None
-        note: each entry in parameter_information must have a long name, a description is recommended and a flag and a default value are completely optional
+        note: each entry in parameter_information must have a long name, value_type and description are recommended and flag and default_value are completely optional
         note: any blank entry should be represented as an empty string"""
 
         # copy arguments into local namespace
@@ -73,10 +86,33 @@ class Interface:
         self.pattern_tree = pattern_tree
 
         # initialise parameters dict to map long names to parameter objects
-        self.parameters = {}
-        # format parameter information into list of parameter classes
+        self.parameters = []
+        # initialise current working path to keep track of where we are in the pattern tree
+        self.current_working_path = self.script_name
+        # iterate over parameter information to assert formatting and add to self.parameters
         for parameter in parameter_information:
-            pass
+
+            # assert there are 5 values for the parameter
+            if len(parameter) != 5 or any(type(value) != str for value in parameter):
+                raise ValueError("Parameter is not valid. It must consist of exactly 5 strings, use an empty string to represent None")
+
+            # assert flag is correctly formatted
+            if parameter[0] != "" and (parameter[0][0] != "-" or len(parameter[0]) != 2 or parameter[0][1] in Interface.control_characters + ("-",)):
+                raise ValueError("Flag '" + parameter[0] + "' is not valid. It must be a dash followed by a single, non-control character")
+
+            # assert long name is correctly formatted
+            if (not parameter[1].startswith("--")) or len(parameter[1]) < 3 or "--" in parameter[1][1:] or any(character in parameter[1] for character in Interface.control_characters):
+                raise ValueError("Long name '" + parameter[1] + "' is not valid. It must be a label starting with '--' followed by any number of non-control characters")
+
+            # assert long name is unique
+            if parameter[1] in self.parameters:
+                raise ValueError("Long name '" + parameter[1] + "' is a duplicate")
+            # assert flag is unique
+            elif parameter[0] in [obj.flag for obj in self.parameters]:
+                raise ValueError("Flag '" + parameter[0] + "' is a duplicate")
+            else:
+                # add parameter to self.parameters
+                self.parameters.append(Parameter(parameter[0], parameter[1], parameter[2], parameter[3], parameter[4]))
 
     # parse given arguments
     def parse(self, given_arguments, assert_typing=True):
@@ -89,13 +125,18 @@ class Interface:
         pass
 
     # display an error
-    def display_error(self):
-        """displays error message to the user
+    def display_error(self, message, error_code=-1):
+        """displays error message to the user, via stderr then exit
         takes:
             STR message - the error to display to the user
+            INT error_code - the error code to exit with
         gives:
             None"""
-        pass
+
+        # print error
+        print(self.current_working_path + ": " + message + "\n\tTry '" + self.current_working_path + " --help' for more information", file=standard_error)
+        # exit with error code
+        exit(error_code)
 
     # display the help message
     def display_help(self):
