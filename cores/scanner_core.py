cc9cdfa750cae055c56513cc85f1fb3a5bc38332
brasspots <44511851+brasspots@users.noreply.github.com>
2018-12-12 22:06:09 +0000

    Delete scanner_core.py
    Removed as functionality has been placed in cores/parser.py

--- cores/scanner_core.py
+++
@@ -1,8 +0,0 @@
-# python3 module used by clihelper to scan patterns
-# this module contains various functions used to scan an input against a pattern string
-#
-# see https://github.com/brasspots/clihelper/tree/master for the latest version
-
-# initialise version
-VERSION = "1.0"
-
