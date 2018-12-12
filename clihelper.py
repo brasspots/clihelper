9e6d22bccf4387c9c99cb5364b699a365a912722
brasspots <44511851+brasspots@users.noreply.github.com>
2018-12-12 20:50:40 +0000

    Filled in all functions apart from display_help

--- clihelper.py
+++ clihelper.py
@@ -93,8 +93,8 @@ class Interface:
         for parameter in parameter_information:
 
             # assert there are 5 values for the parameter
-            if len(parameter) != 5 or any(type(value) != str for value in parameter):
-                raise ValueError("Parameter is not valid. It must consist of exactly 5 strings, use an empty string to represent None")
+            if len(parameter) != 5:
+                raise ValueError("Parameter is not valid. It must consist of exactly 5 values")
 
             # assert flag is correctly formatted
             if parameter[0] != "" and (parameter[0][0] != "-" or len(parameter[0]) != 2 or parameter[0][1] in Interface.control_characters + ("-",)):
@@ -122,7 +122,66 @@ class Interface:
             BOOL assert_typing - whether to error on a type mismatch according to cores/checker.py
         gives:
             DICT - every possible long name as the key and its value as the value"""
-        pass
+
+        # initialise the working branch and the branch keys
+        current_branch = self.pattern_tree
+        argument_branch_index = 0
+
+        # find the correct pattern in the pattern tree
+        while type(current_branch) == dict:
+
+            # check for --help
+            if given_arguments[argument_branch_index] == "--help":
+                self.display_help()
+
+            # error if the next argument is not a valid branch
+            elif given_arguments[argument_branch_index] not in current_branch:
+                self.display_error("Unknown sub-command '" + given_arguments[argument_branch_index] + "'")
+
+            else:
+
+                # update the current working branch
+                if argument_branch_index != 0:
+                    self.current_working_path += " " + given_arguments[argument_branch_index]
+
+                # update the current branch and argument branch index
+                current_branch = current_branch[given_arguments[argument_branch_index]]
+                argument_branch_index += 1
+
+        # check for help
+        if "--help" in given_arguments[argument_branch_index:]:
+            self.display_help()
+
+        # scan the pattern
+        scan_results, scan_errors = parser.scan_pattern(current_branch, given_arguments[argument_branch_index:])
+
+        # load results into parameters
+        for obj in self.parameters:
+            # check if obj was updated in the scan and update obj.value
+            if obj.long_name in scan_results:
+                obj.value = scan_results[obj.long_name]
+
+        # check for missing required argument errors
+        if scan_errors["missing_required"]:
+            self.display_error("Missing required argument" + "s '" if len(scan_errors["missing_required"]) > 1 else " '" + ", ".join(scan_errors["missing_required"]) + "'")
+
+        # check for missing argument errors
+        if scan_errors["missing_argument"]:
+            self.display_error("Missing value for argument" + "s '" if len(scan_errors["missing_argument"]) > 1 else " '" + ", ".join(scan_errors["missing_argument"]) + "'")
+
+        # check for unknown arguments
+        if scan_errors["unknown"]:
+            self.display_error("Unknown argument" + "s '" if len(scan_errors["unknown"]) > 1 else " '" + ", ".join(scan_errors["unknown"]) + "'")
+
+        # check types of all parameters if assert_typing is on
+        if assert_typing:
+            for obj in self.parameters:
+                # if the type is not correct then display an error
+                if not obj.assert_type():
+                    self.display_error("Argument value '" + str(obj.value) + "' did not meet type " + obj.value_type + " for argument " + obj.long_name)
+
+        # return scan results
+        return {obj.long_name: obj.value for obj in self.parameters}
 
     # display an error
     def display_error(self, message, error_code=-1):
@@ -140,12 +199,14 @@ class Interface:
 
     # display the help message
     def display_help(self):
-        """displays information and usage patterns to user
+        """displays information and usage patterns to user then exit
         takes:
             None
         gives:
             None"""
-        pass
+
+        # exit with successful completion
+        exit(0)
 
     # open a file
     def open_file(self, path, mode):
@@ -155,4 +216,44 @@ class Interface:
             STR mode - the file will be opened in this mode
         gives:
             child of io.IOBase - the file object, the same thing returned from open() with the specified mode"""
-        pass
+
+        # potential file overwrite
+        if path_check.isfile(path) and "w" in mode:
+            # acquire overwrite confirmation
+            user_confirmation = input(self.current_working_path + ": File " + path + " already exists, overwrite? [y/n]: ").upper()
+
+            while user_confirmation not in ("Y", "N"):
+                print(self.current_working_path + ": Please enter 'Y' or 'N'")
+                user_confirmation = input(self.current_working_path + ": File " + path + " already exists, overwrite? [y/n]: ").upper()
+
+            # check for overwrite denial
+            if user_confirmation == "N":
+                exit(0)
+            # implicit continue
+
+        # attempt to open the file
+        try:
+            file = open(path, mode)
+
+        # permission denied
+        except PermissionError:
+            self.display_error("Permission denied when accessing file " + path)
+            exit(1)
+
+        # file not found
+        except FileNotFoundError:
+            self.display_error("Could not locate file " + path)
+            exit(1)
+
+        # file exists
+        except FileExistsError:
+            self.display_error("File " + path + " already exists")
+            exit(1)
+
+        # catch all
+        except:
+            self.display_error("Could not access file " + path)
+            exit(1)
+
+        # return IO object
+        return file
